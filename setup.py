003b89a6579f575ee0a6ae31af3e978846929d1d
Doris Tang <tangxingqin@gmail.com>
2020-10-20 17:30:07 +0800

    * Wechat payments v0.3.1   * Change version of package "requests" since conflict.

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 install_requires = [
-    'requests==2.9.1',
+    'requests>=2.9.1',
     'xmltodict==0.10.1',
 ]
 
 setup(
     name='wechat_pay',
-    version='0.3.0',
+    version='0.3.1',
     packages=find_packages(),
     include_package_data=True,
     install_requires=install_requires,
